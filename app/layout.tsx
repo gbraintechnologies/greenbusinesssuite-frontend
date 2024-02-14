90728ea4746ee95aaf64bf0e9441e8e60df61e5c
Okraks <antwisaac7@gmail.com>
2024-02-14 16:13:27 +0000

    Updating favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mesh Suite Forms",
-  description: "Mesh Suite Forms",
+  title: "Mesh Suite",
+  description: "Mesh Suite",
 };
 
 // Toast Notifications
@@ -24,6 +24,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="favicon/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
       <body className={inter.className}>
         <ReactQueryProvider>
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Mesh Suit forms</div>;
+  return <div>Mesh Suit</div>;
 }
