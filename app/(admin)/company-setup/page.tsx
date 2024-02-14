380b732e7457339a6659c8ddff72ab68ef2ed5dd
Okraks <antwisaac7@gmail.com>
2024-02-14 16:24:39 +0000

    Setup complete with UI provided

---
+++ app/(admin)/company-setup/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CompanySetup() {
+  return <div>CompanySetup</div>;
+}
+
+export default CompanySetup;
---
+++ app/(admin)/jurisdiction-setup/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JurisdictionSetup() {
+  return <div>JurisdictionSetup</div>;
+}
+
+export default JurisdictionSetup;
