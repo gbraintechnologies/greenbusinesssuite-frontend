fdd8c2d3ef02e2a5b6cf24385dcf6041e2a56f9e
Ernest Kojo Amenyedzi <eamenyedzi@logicielghana.com>
2024-02-27 17:57:35 +0000

    entrypoint.sh edited online with Bitbucket

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Check that we have NEXT_PUBLIC_BACKEND_URL vars"
 test -n "$NEXT_PUBLIC_BACKEND_URL"
 
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_BACKEND_URL#$NEXT_PUBLIC_BACKEND_URL#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_API_URL#$NEXT_PUBLIC_API_URL#g"
 
 
 echo "Starting Nextjs"
