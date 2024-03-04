46a936b2b95e137e3de4b00f78c571ea262d6dae
Sidney <atokwamina77@gmail.com>
2024-03-04 14:37:13 +0000

    Update on set new password

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -63,12 +63,10 @@ function LogIn() {
       if (token?.status === 200) {
         addAdminData(token?.data);
         const user = await fetchCurrentUser(token.data?.access_token);
-        // alert(JSON.stringify(user))
+         console.log(JSON.stringify(user))
         addAdminData(user?.data);
         if (
-          user?.data.user_status === "NEWLY_CREATED"
-          // ||
-          // user?.data.user_status === "TEMP_CREDENTIALS"
+          user?.data.user_status === "TEMP_CREDENTIALS"
         ) {
           toast("Create your password");
           router.push(`/create-password`);
