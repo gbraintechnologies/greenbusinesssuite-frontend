bb2820085fdd3fda189ffb5a90baeb987ef4ff7b
Okraks <antwisaac7@gmail.com>
2024-05-29 16:06:10 +0000

    minor updates

--- services/features/jurisdictionsService.tsx
+++ services/features/jurisdictionsService.tsx
@@ -1,11 +1,9 @@
 import authApi from "../meshAuthClient";
 
-
 export const allJurisdictions = () => {
-    return () =>
-        authApi.get("/jurisdictions/list").then((res) => res.data);
+  return () => authApi.get("/jurisdictions/list").then((res) => res.data);
 };
 
 export const createJurisdictions = (data: any) => {
-    return authApi.post("/jurisdictions/create", data);
+  return authApi.post("/jurisdictions/create", data);
 };
