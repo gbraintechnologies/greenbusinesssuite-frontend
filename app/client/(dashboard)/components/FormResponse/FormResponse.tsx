e49885b6ea24fc9abf98e6f941c9279322b03069
Philemon Forson <forsonphilemon1@gmail.com>
2024-07-05 16:18:22 +0000

    client form download fix

--- app/client/(dashboard)/components/FormResponse/FormResponse.tsx
+++ app/client/(dashboard)/components/FormResponse/FormResponse.tsx
@@ -10,11 +10,15 @@ type Props = {
 const renderFormResponse = (formField: any) => {
   switch (formField?.fieldDataType) {
     case "short-text":
-      return <input defaultValue={formField?.response} readOnly />;
+      return <input defaultValue={formField?.response} className="fit" readOnly />;
     case "email":
-      return <input defaultValue={formField?.response} className="email-input" readOnly />;
+      return <input defaultValue={formField?.response} className="fit" readOnly />;
     case "number":
-      return <input defaultValue={formField?.response} readOnly />;
+      return <input defaultValue={formField?.response} className="fit" readOnly />;
+    case "checkboxes":
+      return <input defaultValue={formField?.response} className="fit" readOnly />;
+    case "dropdown":
+      return <input defaultValue={formField?.response} className="fit" readOnly />;
     case "phone":
       return (
         <PhoneSelector
