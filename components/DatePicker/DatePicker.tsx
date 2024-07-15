0cea50bb3bdf543856ab2bffde3571bccc214319
Okraks <antwisaac7@gmail.com>
2024-07-15 14:29:52 +0000

    Datepicker components updates

--- components/DatePicker/DatePicker.tsx
+++ components/DatePicker/DatePicker.tsx
@@ -4,20 +4,21 @@ import { BsChevronDown } from "react-icons/bs";
 
 const DatePicker = () => {
   return (
-    <div className="flex shadow-[0px_2px_8px_0px_rgba(100, 116, 139, 0.1)] bg-white border border-[#E2E8F0] w-fit rounded-lg">
-      <button className="flex justify-between items-center px-3 py-1 border-r border-[#E2E8F0] gap-2">
-        <div className="text-sm">Today</div>
-        <BsChevronDown color="#94A3B8" />
+    <></>
+    // <div className="flex shadow-[0px_2px_8px_0px_rgba(100, 116, 139, 0.1)] bg-white border border-[#E2E8F0] w-fit rounded-lg">
+    //   <button className="flex justify-between items-center px-3 py-1 border-r border-[#E2E8F0] gap-2">
+    //     <div className="text-sm">Today</div>
+    //     <BsChevronDown color="#94A3B8" />
 
-      </button>
-      <button className="flex gap-3 items-center px-3 py-1">
-        <div>
-          <DatePickerIcon />
-        </div>
-        <div className="text-sm">All Time</div>
-        <BsChevronDown color="#94A3B8" />
-      </button>
-    </div>
+    //   </button>
+    //   <button className="flex gap-3 items-center px-3 py-1">
+    //     <div>
+    //       <DatePickerIcon />
+    //     </div>
+    //     <div className="text-sm">All Time</div>
+    //     <BsChevronDown color="#94A3B8" />
+    //   </button>
+    // </div>
   );
 };
 
