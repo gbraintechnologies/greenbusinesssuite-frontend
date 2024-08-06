4df112b9806a2d00868ec014b7186059c24513b3
Okraks <antwisaac7@gmail.com>
2024-08-06 14:33:46 +0000

    deactivated page set up

---
+++ app/[tenantId]/deactivated/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Deactivated() {
+  return (
+    <div>
+      <p>Implement Deactivated UI in figma here</p>
+    </div>
+  );
+}
+
+export default Deactivated;
