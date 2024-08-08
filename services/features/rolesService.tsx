d80e4b168159d6e2eb104c592609d82dac9a66a5
Sidney <atokwamina77@gmail.com>
2024-08-08 09:05:32 +0000

    Edit role int

--- services/features/rolesService.tsx
+++ services/features/rolesService.tsx
@@ -14,10 +14,35 @@ export const createRole = ({ name, description }: any) => {
   });
 };
 
+export const EditRole = ({ id, name, description }: any) => {
+  return authApi.post("/apps/roles/edit", {
+    id,
+    role_name: name,
+    app_id: 1,
+    role_description: description,
+    is_support: true,
+    is_admin_role: true
+  });
+};
+
 export const assignRoleToUser = (userID: any, roleID: any) => {
   return authApi.post("/users/create_user_profile", {
     user_id: userID,
     app_id: 1,
     role_id: roleID,
   });
+};
+
+export const allUserRoles = (limit: number = 100) => {
+  return () =>
+    authApi
+      .get(`/apps/all_roles/?limit=${limit}`)
+      .then((res) => res.data);
+};
+
+export const RoleByID = (RoleID: number) => {
+  return () =>
+    authApi
+      .get(`/apps/roles/${RoleID}`)
+      .then((res) => res.data);
 };
