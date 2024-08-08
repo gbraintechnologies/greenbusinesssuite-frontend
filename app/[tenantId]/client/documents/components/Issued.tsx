71429a91bb9bc656e721aafb29d2066ca1c95c5b
Okraks <antwisaac7@gmail.com>
2024-08-08 14:04:35 +0000

    Updates to issuing docs to client

--- app/[tenantId]/client/documents/components/Issued.tsx
+++ app/[tenantId]/client/documents/components/Issued.tsx
@@ -20,58 +20,6 @@ function Issued() {
 
   const { auth } = useAuth();
 
-  const [issued, setIssued] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-
-  const fetchIssued = async (uniqueCompanyIds: any) => {
-    setLoading(true);
-    let temp: any = [];
-    for (let i = 0; i < uniqueCompanyIds.length; i++) {
-      try {
-        await services
-          .getAllIssuedDocsRaw(user?.id, uniqueCompanyIds[i])
-          .then((res) => {
-            temp.push(...res.data);
-          });
-      } catch (e) {
-        //
-      }
-    }
-
-    setIssued(temp);
-    setLoading(false);
-  };
-
-  // // TODO: Get company id from user profile after major changes
-
-  // // completed forms
-  // const { data: completedForms, isLoading: areCompletedFormsLoading } =
-  //   useQuery({
-  //     queryKey: ["get completed forms by user", user?.id],
-  //     queryFn: services.getCompletedFormsByUserId(user?.id),
-  //     enabled: Boolean(user?.id),
-  //   });
-
-  // // TODO: TEMPORAL WAY OF FETCHING TO GET ALL ISSUED FROM ALL COMPANIES - AFTER MULTITENANCY CHANGE
-  // useEffect(() => {
-  //   //
-  //   if (completedForms?.length > 0) {
-  //     setIssued([]);
-
-  //     const uniqueCompanyIds = [
-  //       // @ts-ignore
-  //       ...new Set(completedForms.map((item: any) => item.companyId)),
-  //     ];
-
-  //     fetchIssued(uniqueCompanyIds);
-  //   }
-  // }, [completedForms, areCompletedFormsLoading]);
-
-  // // TODO: Get companies from user form responses
-  // let companyId = 7;
-
-  //
   const {
     data: issuedDocsForClient,
     isLoading,
@@ -86,7 +34,7 @@ function Issued() {
     refetch();
   }, []);
 
-  if (loading) {
+  if (isLoading) {
     return (
       <div className="grid grid-cols-4 gap-10">
         <DocumentSkeleton />
@@ -99,13 +47,14 @@ function Issued() {
 
   return (
     <div>
-      {(issued == undefined || issued?.length === 0) && (
+      {(issuedDocsForClient == undefined ||
+        issuedDocsForClient?.length === 0) && (
         <NoDocuments text="No Issued Documents" />
       )}
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
         {issuedDocsForClient &&
-          issued?.map((document: any) => {
+          issuedDocsForClient?.map((document: any) => {
             return (
               <DocumentCard document={document} key={document?.fileName} />
             );
