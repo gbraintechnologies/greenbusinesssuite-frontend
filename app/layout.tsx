36541972fe7db241727981dc4089d29e6c2d682b
Okraks <antwisaac7@gmail.com>
2024-08-28 13:00:28 +0000

    Layout title update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mesh Suite | %s",
+  title: "Mesh Suite",
   description: "Mesh Suite",
   icons: {
     icon: "/favicon.ico",
