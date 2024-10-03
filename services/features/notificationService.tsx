a74c813d45024d8c881ecb51476c4459748d6725
Okraks <antwisaac7@gmail.com>
2024-10-03 13:47:23 +0000

    Send email endpoint

--- services/features/notificationService.tsx
+++ services/features/notificationService.tsx
@@ -1,5 +1,13 @@
 import defaultMeshApi from "../defaultMeshClient";
 
+// send notifications
+export const sendEmail = () => {
+  return () =>
+    defaultMeshApi.get(`/notifications/email/push`).then((res) => res.data);
+};
+
+// retrieve messages
+
 export const allPastNotifications = () => {
   return () =>
     defaultMeshApi
