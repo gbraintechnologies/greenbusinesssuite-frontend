43143783dff0d33096f49880b9079ccc4f7a957d
Okraks <antwisaac7@gmail.com>
2024-12-03 15:12:29 +0000

    Minor updates

--- app/(admin)/(pages)/category-setup/[categoryId]/create-module/page.tsx
+++ app/(admin)/(pages)/category-setup/[categoryId]/create-module/page.tsx
@@ -6,39 +6,36 @@ import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import ModuleForm from "../../components/ModuleForm";
 
-const page = ({params}: any) => {
+const page = ({ params }: any) => {
   // getting the category id
   let categoryId = params.categoryId;
 
   // states for handling submission
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const router = useRouter();
 
   const submitFn = (values: any, formikHelpers: FormikHelpers<any>): void => {
-
-
     // Check if both fields are empty
     if (!values.companyAdminPortal && !values.clientPortal) {
       toast.error(
         "Please fill out at least one of the 'Company Admin Portal' or 'Client Portal' fields before submitting."
       );
-      
-      
+
       return;
     }
 
     try {
-      setIsSubmitting(true)
+      setIsSubmitting(true);
       console.log("Submitting form with values:", values);
       formikHelpers.resetForm();
-      router.back()
+      router.back();
     } catch (error) {
-      setIsSubmitting(false)
+      setIsSubmitting(false);
       console.error("Error submitting form", error);
       toast.error("An error occurred. Please try again");
     } finally {
-      setIsSubmitting(false)
+      setIsSubmitting(false);
     }
   };
 
