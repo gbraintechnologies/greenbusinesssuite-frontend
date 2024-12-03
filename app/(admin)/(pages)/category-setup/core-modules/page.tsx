c0d2ac92f281eb2523d602dd24c9ed0594b1bba4
Philemon Forson <firstkoolaid21@gmail.com>
2024-12-03 16:49:15 +0000

    minor edit

--- app/(admin)/(pages)/category-setup/core-modules/page.tsx
+++ app/(admin)/(pages)/category-setup/core-modules/page.tsx
@@ -15,49 +15,6 @@ function CreateModule() {
 
   useEffect(() => {}, [modules]);
 
-<<<<<<< HEAD
-    useEffect(() => {
-    }, [modules]);
-
-    return (
-        <div className="w-full px-5 pb-20 py-5">
-            <div className="w-full text-primary-dark flex justify-between">
-                <div>
-                    <h3 className="font-semibold text-xl mb-10">Core Modules</h3>
-                </div>
-                <div>
-                    <Link href="/category-setup/core-modules/create" className="bg-primary-green flex text-white text-sm px-4 hover:opacity-95 items-center gap-2 rounded-xl">
-                        <IoIosAddCircleOutline /> Create new module
-                        <div className="border-opacity-50 border-white h-10"></div>
-                    </Link>
-
-                </div>
-            </div>
-            <div>
-                <Link
-                    href="/category-setup"
-                    className="bg-white border border-gray-200 flex text-black text-sm px-2 hover:bg-gray-100 py-1 hover:opacity-95 items-center gap-2 rounded-xl w-24"
-                >
-                    <IoArrowBackSharp />&nbsp;Back
-                    <div className="border-opacity-50 border-white h-10"></div>
-                </Link>
-            </div>
-            <div className="max-w-full">
-                <div className="w-full p-6 grid grid-cols-3 gap-6">
-                    {modules && modules.map((item: any) => (
-                        <Link
-                            key={item.id}
-                            href={`/category-setup/core-modules/${item.id}`} // Pass the category id as a query parameter
-                        >
-                            <CardDescription
-                                name={item.moduleName}
-                                description={item.moduleDescription}
-                            />
-                        </Link>
-                    ))}
-                </div>
-            </div>
-=======
   return (
     <div className="w-full px-5 pb-20 py-5">
       <div className="w-full text-primary-dark flex justify-between">
@@ -69,7 +26,6 @@ function CreateModule() {
             <IoArrowBackSharp />
           </Link>
           <h3 className="font-semibold text-2xl my-4">Core Modules</h3>
->>>>>>> 43143783dff0d33096f49880b9079ccc4f7a957d
         </div>
 
         <div>
@@ -89,7 +45,7 @@ function CreateModule() {
             modules.map((item: any) => (
               <Link
                 key={item.id}
-                href={`/category-setup/1?moduleId=${item.id}`} // Pass the category id as a query parameter
+                href={`/category-setup/core-modules/${item.id}`} // Pass the category id as a query parameter
               >
                 <CardDescription
                   name={item.moduleName}
