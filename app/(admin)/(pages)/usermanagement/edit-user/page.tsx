a6a3c1f788c6120b6fedbd7ea03cbf2bfad68f1b
Philemon Forson <firstkoolaid21@gmail.com>
2024-12-12 14:02:25 +0000

    use ref initial value error fixed

--- app/(admin)/(pages)/usermanagement/edit-user/page.tsx
+++ app/(admin)/(pages)/usermanagement/edit-user/page.tsx
@@ -90,7 +90,7 @@ function page() {
     ); // For Profile Picture
   }, [data]);
 
-  const inputFileRef = React.useRef();
+  const inputFileRef = React.useRef(null);
 
   const editUser = async (values: any, resetForm: any) => {
     let finalData = {
