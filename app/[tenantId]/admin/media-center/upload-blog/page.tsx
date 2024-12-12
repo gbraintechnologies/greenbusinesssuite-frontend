4ceb755c7d5a45299963ad0f012b28bc6893ea88
Sidney <atokwamina77@gmail.com>
2024-12-12 13:18:01 +0000

    Updates on UIs

---
+++ app/[tenantId]/admin/media-center/upload-blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UploadBlog() {
+  return (
+    <div className="px-5 mt-10 pb-10">
+      <h3 className="font-semibold mb-8 text-xl">UploadBlog</h3>
+    </div>
+  );
+}
+
+export default UploadBlog;
