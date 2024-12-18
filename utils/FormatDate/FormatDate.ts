2097ba83c8d7fba0d97423b45834e43e8fe78201
Philemon Forson <firstkoolaid21@gmail.com>
2024-12-18 23:19:49 +0000

    edited date format

--- utils/FormatDate/FormatDate.ts
+++ utils/FormatDate/FormatDate.ts
@@ -17,7 +17,7 @@ export const FormatDateTime = (date: any) => {
   if (Boolean(date)) return format(new Date(date), "HH:mm, do MMM, yyyy");
 };
 
-export const  FormatDateWithSuffix = (date: any) =>  {
+export const  FormatDateWithSuffix = (date: any, type: "default" | "text" = "default") =>  {
   const day = format(date, 'd'); 
   const monthAndYear = format(date, 'MMM, yyyy');
   const time = format(date, 'hh:mm a'); 
@@ -38,7 +38,7 @@ export const  FormatDateWithSuffix = (date: any) =>  {
   };
 
   const dayWithSuffix = `${day}${getOrdinalSuffix(day)}`;
-  return `${dayWithSuffix} ${monthAndYear}, ${time}`;
+  return type == "default" ? `${dayWithSuffix} ${monthAndYear}, ${time}` : `${dayWithSuffix} ${monthAndYear} at ${time}`;
 }
 
 export default FormatDate;
