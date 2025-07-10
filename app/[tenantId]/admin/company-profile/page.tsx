28525e95740451bce83fae97aabb782ec6c16ef6
Okraks <antwisaac7@gmail.com>
2025-07-10 21:51:12 +0000

    Sketch Color Bug Fix

--- app/[tenantId]/admin/company-profile/page.tsx
+++ app/[tenantId]/admin/company-profile/page.tsx
@@ -15,8 +15,6 @@ import UserIcon from "@/public/icons/UserIcon";
 import LoadingIcon from "@/components/LoadingIcon/LoadingIcon";
 import { IFilter, TimelineType, TimelineValues } from "@/types";
 
-// import { SketchPicker } from "react-color";
-
 import WriteIcon from "@/public/icons/WriteIcon";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import CloudUploadIcon from "@/public/icons/CloudUploadIcon";
@@ -470,13 +468,7 @@ const Page = (props: any) => {
                         <p className="p-2">{color}</p>
                       </button>
                     )}
-                    {showColorPicker && (
-                      <></>
-                      // <SketchPicker
-                      //   color={color}
-                      //   onChangeComplete={handleChangeComplete}
-                      // />
-                    )}
+                    {showColorPicker && <></>}
                   </div>
                 </div>
               </div>
